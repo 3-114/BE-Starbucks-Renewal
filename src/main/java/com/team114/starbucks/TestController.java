5b07299dd0df77145aa5efd921d6fb4646ea0929
So-ko-nyun <cn6867@naver.com>
2025-03-26 21:06:57 +0900

    ci/cd test9

--- src/main/java/com/team114/starbucks/TestController.java
+++ src/main/java/com/team114/starbucks/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String home() {
-        return "Hello World! ci/cd test! 03/26";
+        return "Hello World! ci/cd test!!! 03/26";
     }
 
 }
