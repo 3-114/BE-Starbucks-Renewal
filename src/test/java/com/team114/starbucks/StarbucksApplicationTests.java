3df41bc9b93e4fd9431b1b25c10742baa759ca1f
EUNSEO-YA <kendyjenny@naver.com>
2025-03-26 14:06:09 +0900

    Project Setting

---
+++ src/test/java/com/team114/starbucks/StarbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team114.starbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
