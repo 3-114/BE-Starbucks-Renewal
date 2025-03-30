02730dda82a85b59064ee217cc518a712afdda60
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-03-30 15:12:37 +0900

    [auth]docs: 주석(swagger url) 추가

--- src/main/java/com/team114/starbucks/common/config/SwaggerConfig.java
+++ src/main/java/com/team114/starbucks/common/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+// Swagger URL : http://localhost:8080/swagger-ui/index.html
 @OpenAPIDefinition(
         info = @io.swagger.v3.oas.annotations.info.Info(
                 title = "114-STARBUCKS-Renewal Service API",
