0f4fca04be1f886ecd0f895291e9f66edb5ae6ea
So-ko-nyun <cn6867@naver.com>
2025-04-10 15:11:51 +0900

    [product]hotfix: productThumbnail, productDescription 서비스 분해

--- src/main/java/com/team114/starbucks/domain/product/application/ProductThumbnailService/ProductThumbnailService.java
+++ src/main/java/com/team114/starbucks/domain/product/application/ProductThumbnailService/ProductThumbnailService.java
@@ -1,11 +1,11 @@
-package com.team114.starbucks.domain.product.application.ProductThumbnailService;
-
-import com.team114.starbucks.domain.product.entity.ProductThumbnail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductThumbnailService {
-
-
-
-
-}
+//package com.team114.starbucks.domain.product.application.ProductThumbnailService;
+//
+//import com.team114.starbucks.domain.product.entity.ProductThumbnail;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface ProductThumbnailService {
+//
+//
+//
+//
+//}
--- src/main/java/com/team114/starbucks/domain/product/application/ProductThumbnailService/ProductThumbnailServiceImpl.java
+++ src/main/java/com/team114/starbucks/domain/product/application/ProductThumbnailService/ProductThumbnailServiceImpl.java
@@ -1,19 +1,19 @@
-package com.team114.starbucks.domain.product.application.ProductThumbnailService;
-
-import com.team114.starbucks.domain.product.entity.ProductThumbnail;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-
-@Service
-@RequiredArgsConstructor
-public class ProductThumbnailServiceImpl implements ProductThumbnailService {
-
-    private final ProductThumbnailService productThumbnailService;
-
-
-
-
-}
+//package com.team114.starbucks.domain.product.application.ProductThumbnailService;
+//
+//import com.team114.starbucks.domain.product.entity.ProductThumbnail;
+//import lombok.NoArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//@RequiredArgsConstructor
+//public class ProductThumbnailServiceImpl implements ProductThumbnailService {
+//
+//    private final ProductThumbnailService productThumbnailService;
+//
+//
+//
+//
+//}
