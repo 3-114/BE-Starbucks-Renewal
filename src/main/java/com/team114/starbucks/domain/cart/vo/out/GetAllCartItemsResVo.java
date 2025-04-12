4aae59c314c1228cc60ab92f4a6c57d4e5f5732d
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-04-13 07:46:53 +0900

    [cart]refactor: GetAllCartItemsResVo 필드 수정

--- src/main/java/com/team114/starbucks/domain/cart/vo/out/GetAllCartItemsResVo.java
+++ src/main/java/com/team114/starbucks/domain/cart/vo/out/GetAllCartItemsResVo.java
@@ -8,34 +8,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GetAllCartItemsResVo {
 
-    private String productName;
-    private Integer productPrice;
-    private String color;
-    private String size;
-    private Long optionPrice;
-    private Integer discountRate;
     private Long quantity;
     private Boolean selected;
     private Boolean valid;
 
     @Builder
     public GetAllCartItemsResVo(
-            String productName,
-            Integer productPrice,
-            String color,
-            String size,
-            Long optionPrice,
-            Integer discountRate,
             Long quantity,
             Boolean selected,
             Boolean valid
     ) {
-        this.productName = productName;
-        this.productPrice = productPrice;
-        this.color = color;
-        this.size = size;
-        this.optionPrice = optionPrice;
-        this.discountRate = discountRate;
         this.quantity = quantity;
         this.selected = selected;
         this.valid = valid;
