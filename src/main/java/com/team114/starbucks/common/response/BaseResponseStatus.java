52c33304e36e5b9befa50d64981898ca00e00b37
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-04-17 14:01:42 +0900

    [common]feat : 에러 코드 넘버 1000번대로 변경

--- src/main/java/com/team114/starbucks/common/response/BaseResponseStatus.java
+++ src/main/java/com/team114/starbucks/common/response/BaseResponseStatus.java
@@ -17,15 +17,6 @@ public enum BaseResponseStatus {
     /**
      * 400 : client 에러
      */
-    WRONG_JWT_TOKEN(HttpStatus.UNAUTHORIZED, false, 401, "다시 로그인 해주세요"),
-    NO_SIGN_IN(HttpStatus.UNAUTHORIZED, false, 402, "로그인을 먼저 진행해주세요"),
-    NO_ACCESS_AUTHORITY(HttpStatus.FORBIDDEN, false, 403, "접근 권한이 없습니다"),
-    DISABLED_USER(HttpStatus.FORBIDDEN, false, 404, "비활성화된 계정입니다. 계정을 복구하시겠습니까?"),
-    FAILED_TO_RESTORE(HttpStatus.INTERNAL_SERVER_ERROR, false, 405, "계정 복구에 실패했습니다. 관리자에게 문의해주세요."),
-    NO_EXIST_OAUTH(HttpStatus.NOT_FOUND, false, 406, "소셜 로그인 정보가 존재하지 않습니다."),
-    NO_EXIST_VALUE(HttpStatus.NOT_FOUND, false, 407, "해당 정보가 존재하지 않습니다."),
-    FAILED_TO_SAVE(HttpStatus.BAD_REQUEST, false, 408, "저장에 실패하였습니다."),
-    FAILED_TO_FIND(HttpStatus.NOT_FOUND, false, 409, "조회에 실패하였습니다."),
 
     /**
      * 500 : server 에러
@@ -36,6 +27,31 @@ public enum BaseResponseStatus {
      */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, 900, "Internal server error"),
     SSE_SEND_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, false, 901, "알림 전송에 실패하였습니다."),
+
+    /**
+     * 1000 : custom error
+     */
+    // common : 1000
+    FAILED_TO_SAVE(HttpStatus.BAD_REQUEST, false, 1001, "저장에 실패하였습니다."),
+    FAILED_TO_FIND(HttpStatus.NOT_FOUND, false, 1002, "조회에 실패하였습니다."),
+
+    // security : 1100
+    WRONG_JWT_TOKEN(HttpStatus.UNAUTHORIZED, false, 1101, "다시 로그인 해주세요"),
+    NO_SIGN_IN(HttpStatus.UNAUTHORIZED, false, 1102, "로그인을 먼저 진행해주세요"),
+    NO_ACCESS_AUTHORITY(HttpStatus.FORBIDDEN, false, 1103, "접근 권한이 없습니다"),
+    DISABLED_USER(HttpStatus.FORBIDDEN, false, 1104, "비활성화된 계정입니다. 계정을 복구하시겠습니까?"),
+    FAILED_TO_RESTORE(HttpStatus.INTERNAL_SERVER_ERROR, false, 1105, "계정 복구에 실패했습니다. 관리자에게 문의해주세요."),
+    NO_EXIST_OAUTH(HttpStatus.NOT_FOUND, false, 1106, "소셜 로그인 정보가 존재하지 않습니다."),
+    NO_EXIST_VALUE(HttpStatus.NOT_FOUND, false, 1107, "해당 정보가 존재하지 않습니다."),
+
+    // member : 1200
+    // product : 1300
+    // option : 1400
+    // delivery : 1500
+    // event, category : 1600
+    // coupon : 1700
+    // order : 1800
+    // payment : 1900
 
     /**
      * 2000: users service error
