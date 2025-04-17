25ccb5d3d92bf25642f29d005286315c3260d8a9
So-ko-nyun <cn6867@naver.com>
2025-04-17 11:23:06 +0900

    [product]feat: ProductController swagger추가

--- src/main/java/com/team114/starbucks/domain/product/presentation/ProductController.java
+++ src/main/java/com/team114/starbucks/domain/product/presentation/ProductController.java
@@ -13,6 +13,8 @@ import com.team114.starbucks.domain.product.vo.in.CreateProductRequestVo;
 import com.team114.starbucks.domain.product.vo.in.DeleteProductRequestVo;
 import com.team114.starbucks.domain.product.vo.in.UpdateProductRequestVo;
 import com.team114.starbucks.domain.product.vo.out.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Slf4j
+@Tag(name = "상품 API", description = "상품 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
@@ -30,6 +32,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "상품 전체 조회", description = "상품 전체를 조회합니다.")
     @GetMapping
     public BaseResponseEntity<List<GetProductResponseDto>> getAllProducts() {
 
@@ -46,6 +49,7 @@ public class ProductController {
 
     }
 
+    @Operation(summary = "상품 단건 조회", description = "상품uuid를 통해 단건 조회합니다.")
     @GetMapping("/{productUuid}")
     public BaseResponseEntity<GetProductByIdResponseVo> getProduct(
             @PathVariable String productUuid
@@ -57,6 +61,7 @@ public class ProductController {
         return new BaseResponseEntity<>(HttpStatus.OK, true, "상품 단건 조회 성공", 200, vo);
     }
 
+    @Operation(summary = "상품 등록", description = "상품과 썸네일을 함께 등록합니다.")
     @PostMapping
     public BaseResponseEntity<CreateProductResponseVo> saveProduct(
             @RequestBody CreateProductRequestVo createProductReqVo
@@ -71,6 +76,7 @@ public class ProductController {
 
     }
 
+    @Operation(summary = "상품 수정", description = "상품 정보와 썸네일을 함께 수정합니다.")
     @PutMapping
     public BaseResponseEntity<Void> updateProduct(
             @RequestBody UpdateProductRequestVo updateProductRequestVo
@@ -83,6 +89,7 @@ public class ProductController {
 
     }
 
+    @Operation(summary = "상품 삭제", description = "상품을 삭제합니다.")
     @DeleteMapping
     public BaseResponseEntity<Void> deleteProduct(
             @RequestBody DeleteProductRequestVo deleteProductRequestVo
@@ -94,6 +101,7 @@ public class ProductController {
         return new BaseResponseEntity<>(HttpStatus.OK, true, "상품 삭제 성공", 200, null);
     }
 
+    @Operation(summary = "상품 프리뷰", description = "(썸네일, 상품명, 가격), 상품 미리보기")
     @GetMapping("/preview/{productUuid}")
     public BaseResponseEntity<GetProductPreviewResponseVo> getProductPreview(
             @PathVariable String productUuid
@@ -105,7 +113,5 @@ public class ProductController {
         return new BaseResponseEntity<>(HttpStatus.OK, true, "상품 미리보기 성공", 200, vo);
 
     }
-
-
 
 }
