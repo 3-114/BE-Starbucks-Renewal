36ce5af8b092378f7a439ef32fac42be4f3c1f9c
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-04-18 17:32:23 +0900

    [product-category]feat: mainCategoryUuid 로 product uuid 리스트 조회

--- src/main/java/com/team114/starbucks/domain/productcategory/infrastructure/ProductCategoryRepository.java
+++ src/main/java/com/team114/starbucks/domain/productcategory/infrastructure/ProductCategoryRepository.java
@@ -1,6 +1,8 @@
 package com.team114.starbucks.domain.productcategory.infrastructure;
 
 import com.team114.starbucks.domain.productcategory.entity.ProductCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ public interface ProductCategoryRepository extends JpaRepository<ProductCategory
 
     List<ProductCategory> findAllProductUuidByEventUuid(String eventUuid);
 
+    Page<ProductCategory> findByMainCategoryUuid(String mainCategoryUuid, Pageable entity);
 }
--- src/main/java/com/team114/starbucks/domain/productcategory/presentation/ProductCategoryController.java
+++ src/main/java/com/team114/starbucks/domain/productcategory/presentation/ProductCategoryController.java
@@ -1,13 +1,16 @@
 package com.team114.starbucks.domain.productcategory.presentation;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.team114.starbucks.common.response.BaseResponseEntity;
 import com.team114.starbucks.domain.productcategory.application.ProductCategoryService;
 import com.team114.starbucks.domain.productcategory.dto.in.CreateProductCategoryReqDto;
+import com.team114.starbucks.domain.productcategory.dto.in.PageParamDto;
 import com.team114.starbucks.domain.productcategory.dto.out.GetAllProductUuidResDto;
 import com.team114.starbucks.domain.productcategory.vo.in.CreateProductCategoryReqVo;
 import com.team114.starbucks.domain.productcategory.vo.out.GetAllProductUuidResVo;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +29,7 @@ public class ProductCategoryController {
      * 4. 상품 카테고리 수정
      * 5. 상품 카테고리 삭제
      * 6. GET request: eventUuid / response: List<productUuid>
+     * 7. product uuid 리스트 조회
      *  */
 
     // 1. 상품 카테고리 생성
@@ -50,4 +54,23 @@ public class ProductCategoryController {
                 productCategoryService.getProductUuidsByEventUuid(eventUuid).stream().map(GetAllProductUuidResDto::toVo).toList());
     }
 
+    /**
+     * 7. mainCategoryUuid 로 product uuid 리스트 조회
+     * @param mainCategoryUuid
+     * @param page
+     * @param size
+     * @return
+     */
+    @Operation(summary = "product uuid 리스트 조회", tags = {"product-category"})
+    @GetMapping("/uuid-list")
+    public BaseResponseEntity<Page<GetAllProductUuidResVo>> getProductUuids(
+            @RequestParam(required = false) String mainCategoryUuid,
+            @RequestParam(required = false, defaultValue = "1") int page,
+            @RequestParam(required = false, defaultValue = "10") int size
+    ) {
+        return new BaseResponseEntity<>(
+                "product uuid 리스트 조회에 성공하였습니다.",
+                productCategoryService.getProductUuids(PageParamDto.of(mainCategoryUuid, page, size)).map(GetAllProductUuidResDto::toVo)
+        );
+    }
 }
